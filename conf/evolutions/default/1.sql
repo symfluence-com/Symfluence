7ba2e719857afe763a58c36779008abc1f96bf1a
Frank Lee <frank.lee@symfluence.com>
2012-02-11 18:38:26 +0800

    Data changes to 1.sql and 2.sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,7 +3,9 @@
 # --- !Ups
 
 -- Create syntax for TABLE 'groups'
-
+drop table if exists users_badges;
+drop table if exists badges;
+drop table if exists brands;
 drop table if exists users_posts;
 drop table if exists users_groups;
 drop table if exists users_followers;
@@ -42,20 +44,9 @@ CREATE TABLE `groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Create syntax for TABLE 'play_evolutions'
-CREATE TABLE `play_evolutions` (
-  `id` int(11) NOT NULL,
-  `hash` varchar(255) NOT NULL,
-  `applied_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `apply_script` text,
-  `revert_script` text,
-  `state` varchar(255) DEFAULT NULL,
-  `last_problem` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'user_badges'
-CREATE TABLE `user_badges` (
+CREATE TABLE `users_badges` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` varchar(32) DEFAULT NULL,
   `badge_id` int(11) DEFAULT NULL,
@@ -64,25 +55,16 @@ CREATE TABLE `user_badges` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Create syntax for TABLE 'user_groups_brands'
-CREATE TABLE `user_groups_brands` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_group_id` int(11) DEFAULT NULL,
-  `brands` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'users'
 CREATE TABLE `users` (
   `id` varchar(32) NOT NULL DEFAULT '',
   `username` varchar(255) DEFAULT NULL,
-  `first name` varchar(255) DEFAULT NULL,
-  `last name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `profile_pic_id` varchar(32) DEFAULT NULL,
-  `mailing address` varchar(255) DEFAULT NULL,
+  `mailing_address` varchar(255) DEFAULT NULL,
   `gender` char(1) DEFAULT NULL,
   `date_of_birth` date DEFAULT NULL,
   `occupation` varchar(255) DEFAULT NULL,
@@ -107,7 +89,7 @@ CREATE TABLE `users_followers` (
   KEY `follower_id` (`follower_id`),
   CONSTRAINT `users_followers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `users_followers_ibfk_2` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB =7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'users_groups'
 CREATE TABLE `users_groups` (
@@ -122,7 +104,7 @@ CREATE TABLE `users_groups` (
   KEY `group_id` (`group_id`),
   CONSTRAINT `users_groups_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `users_groups_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`)
-) ENGINE=InnoDB =61 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'users_posts'
 CREATE TABLE `users_posts` (
@@ -137,11 +119,14 @@ CREATE TABLE `users_posts` (
   KEY `users_posts_groups_ibfk1` (`group_id`),
   CONSTRAINT `users_posts_groups_ibfk1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
   CONSTRAINT `users_posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB =49 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 -- Create syntax for TABLE 'groups'
 # --- !Downs
+drop table if exists users_badges;
+drop table if exists badges;
+drop table if exists brands;
 drop table if exists users_posts;
 drop table if exists users_groups;
 drop table if exists users_followers;
